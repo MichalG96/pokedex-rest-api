c5eafc39b08e02c526806cee3ae3a75f5195e5f5
Michal Grochecki <michalgrochecki@gmail.com>
2021-05-28 14:01:13 +0200

    Create database models

--- manage_pokemons/models.py
+++ manage_pokemons/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length=60)
+    type1 = models.CharField(max_length=30)
+    type2 = models.CharField(max_length=30)
+    # Currently, there are
+    generation = models.PositiveSmallIntegerField(validators=[MinValueValidator(1)])
+    legendary = models.BooleanField()
+    stats = models.ForeignKey('Stats', on_delete=models.CASCADE)
+
+
+class Stats(models.Model):
+    total = models.PositiveSmallIntegerField()
+    hp = models.PositiveSmallIntegerField()
+    attack = models.PositiveSmallIntegerField()
+    defense = models.PositiveSmallIntegerField()
+    special_attack = models.PositiveSmallIntegerField()
+    special_defense = models.PositiveSmallIntegerField()
+    speed = models.PositiveSmallIntegerField()
--- pokedex/settings.py
+++ pokedex/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'manage_pokemons.apps.ManagePokemonsConfig',
 ]
 
 MIDDLEWARE = [
