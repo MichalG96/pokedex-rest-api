40c9038fd7de831ece5e49880c77b1037816f958
Michal Grochecki <michalgrochecki@gmail.com>
2021-05-28 16:13:45 +0200

    Evaluate field "total" on save

--- manage_pokemons/models.py
+++ manage_pokemons/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator
+from django.db.models import F
 
 
 class Pokemon(models.Model):
@@ -15,7 +16,13 @@ class Pokemon(models.Model):
     special_attack = models.PositiveSmallIntegerField(default=50)
     special_defense = models.PositiveSmallIntegerField(default=50)
     speed = models.PositiveSmallIntegerField(default=50)
-    total = models.PositiveSmallIntegerField(default=50)
+    total = models.PositiveSmallIntegerField()
+
+    def save(self, *args, **kwargs):
+        stats_sum = (self.hp + self.attack + self.defense + self.special_attack
+        + self.special_defense + self.speed)
+        self.total = stats_sum
+        super(Pokemon, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
