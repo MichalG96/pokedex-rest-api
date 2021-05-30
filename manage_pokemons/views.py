3c3c8261a8bccf6384e24e3f0cb479e1570e3045
Michal Grochecki <michalgrochecki@gmail.com>
2021-05-31 00:29:32 +0200

    Add pagination

--- manage_pokemons/views.py
+++ manage_pokemons/views.py
@@ -7,5 +7,5 @@ from .serializers import PokemonSerializer
 class PokemonViewSet(viewsets.ModelViewSet):
     serializer_class = PokemonSerializer
     queryset = Pokemon.objects.all()
-    filterset_fields = ['type1', 'type2']
-    search_fields = ['name']
+    filterset_fields = ['type1', 'type2', 'name']
+    search_fields = ['type1', 'type2', 'name']
--- pokedex/settings.py
+++ pokedex/settings.py
@@ -4,7 +4,6 @@ import json
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -18,7 +17,6 @@ SECRET_KEY = passes.get('secret_key')
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -64,7 +62,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pokedex.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -74,7 +71,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -94,7 +90,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -107,7 +102,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
@@ -123,5 +117,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter'
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
 }
