5b238cdbfc96f69c5fb230e7b9d4459f54aa04e8
Michal Grochecki <michalgrochecki@gmail.com>
2021-05-30 23:21:09 +0200

    Get total Pokemon stats by using SerializerMethodField

--- manage_pokemons/serializers.py
+++ manage_pokemons/serializers.py
@@ -4,7 +4,13 @@ from .models import Pokemon
 
 
 class PokemonSerializer(serializers.ModelSerializer):
+    total = serializers.SerializerMethodField()
+
     class Meta:
         model = Pokemon
         # exclude = ['total']
         fields = '__all__'
+
+    def get_total(self, obj):
+        return (obj.hp + obj.attack + obj.defense + obj.special_attack
+                + obj.special_defense + obj.speed)
