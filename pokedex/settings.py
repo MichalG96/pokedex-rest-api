2a52632a69995de268cacbe714c5fb72408f19cf
Michal Grochecki <michalgrochecki@gmail.com>
2021-06-01 22:18:54 +0200

    Update settings

--- pokedex/settings.py
+++ pokedex/settings.py
@@ -7,9 +7,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-with open('sensitive_data/passes.json') as f:
-    passes = json.load(f)
-
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECRET_KEY was stored in another, secret file, but because it's a recruitment task,
